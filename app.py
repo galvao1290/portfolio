320dafd20be679ece58064ea1304b2488661d77a
galvao1290 <miguelgalvao10@edu.unifil.br>
2025-02-07 13:44:52 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
